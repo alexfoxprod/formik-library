bf60407fea268c27dd30d992ed328b4c07b180f2
Oleksandr Lutsiuk <oleksandr.lutsiuk@gmail.com>
2023-06-21 19:09:39 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import YouTubeForm from "./components/YouTubeForm";
+import React from "react";
+import { NumericFormat } from "react-number-format";
 
 function App() {
   return (
     <div className="App">
       <YouTubeForm />
+      <NumericFormat
+        value={43414123.4324}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <NumericFormat
+        format="(###) ###-##-##"
+        mask="_"
+        placeholder="(380) 456-12-32"
+      />
     </div>
   );
 }
